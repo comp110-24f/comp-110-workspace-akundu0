9d71cc2e1e13844dea4da5cfb8de32a3765d4f1b
akundu0 <akundu@unc.edu>
2024-10-04 11:26:29 -0400

    Finished exercise 4

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,32 +1,37 @@
-"""List utils"""
+"""Functions that modify lists."""
 
 __author__ = "730762344"
 
 
 def all(a_list: list[int], num: int) -> bool:
+    """Checks if all items in a list are equivalent to an inputted number."""
     is_same: bool = False
     i: int = 0
     while i < len(a_list):
         if a_list[i] == num:
             is_same = True
         else:
-            is_same = False
+            return False
         i += 1
     return is_same
 
 
 def max(a_list: list[int]) -> int:
+    """Returns the largest value in a list."""
     if len(a_list) == 0:
         raise ValueError("max() arg is an empty List")
     i: int = 0
-    largest: int = 0
+    largest: int = a_list[0]
 
     while i < len(a_list):
         if a_list[i] > largest:
             largest = a_list[i]
+        i += 1
+    return largest
 
 
 def is_equal(list_1: list[int], list_2: list[int]) -> bool:
+    """Returns True if two lists are deeply equal."""
     if len(list_1) != len(list_2):
         return False
     else:
@@ -40,6 +45,7 @@ def is_equal(list_1: list[int], list_2: list[int]) -> bool:
 
 
 def extend(list_1: list[int], list_2: list[int]) -> None:
+    """Appends a list onto another list."""
     i: int = 0
 
     while i < len(list_2):
