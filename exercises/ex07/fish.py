ae176d30ec25b60f66eabbe4aa204bfcfe91a35c
akundu0 <akundu@unc.edu>
2024-11-03 19:09:15 -0500

    ex07 backup 2

--- exercises/ex07/fish.py
+++ exercises/ex07/fish.py
@@ -2,10 +2,8 @@
 
 
 class Fish:
-    age: int
 
     def __init__(self):
-        self.age = 0
         return None
 
     def one_day(self):
--- exercises/ex07/river_simulation.py
+++
@@ -1,7 +0,0 @@
-""""""
-
-from exercises.ex07.river import River
-
-my_river = River(10, 2)
-
-my_river.view_river()
